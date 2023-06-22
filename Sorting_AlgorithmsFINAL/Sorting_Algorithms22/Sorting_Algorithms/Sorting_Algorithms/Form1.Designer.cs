433b8c3485c2a2b862a44a82887c2fd21a0982e0
cyhate <103992939+cyhate@users.noreply.github.com>
2023-06-22 11:28:26 +0300

    Add files via upload

---
+++ Sorting_AlgorithmsFINAL/Sorting_Algorithms22/Sorting_Algorithms/Sorting_Algorithms/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Sorting_Algorithms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate Numbers";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(198, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(248, 146);
+            this.textBox1.TabIndex = 1;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(113, 17);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            2000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(70, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Numbers";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Bubble Sort",
+            "Insertion Sort",
+            "Selection Sort"});
+            this.comboBox1.Location = new System.Drawing.Point(24, 83);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(159, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.Text = "Bubble Sort";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(24, 124);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 34);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Sort Numbers";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(198, 179);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(248, 146);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(130, 179);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(53, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Actual Steps:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 208);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Theoretical Steps:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(130, 205);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(53, 20);
+            this.textBox4.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 433);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Sorting Algorithms";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ Sorting_AlgorithmsFINAL/Sorting_Algorithms22/Sorting_Algorithms/Sorting_Algorithms/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sorting_Algorithms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        int NumberofNumbers;
+        int[] UnsortedNumbers;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            Random A = new Random();
+
+            NumberofNumbers = (int)numericUpDown1.Value;
+            UnsortedNumbers = new int[NumberofNumbers];
+
+            textBox1.Text = "";
+
+            for (int i=0; i< NumberofNumbers; i++)
+            {
+
+                UnsortedNumbers[i] = A.Next(90000); //90000 einai o megistos arithos pou tha paraxthe
+                textBox1.Text += UnsortedNumbers[i].ToString() + "\t";
+
+            }
+
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+
+            textBox3.Text = "";
+            int Steps = 0;
+            int theoSteps = 0;
+
+            if (comboBox1.Text == "Selection Sort")
+            {
+                theoSteps = NumberofNumbers * (NumberofNumbers - 1) / 2;
+
+                for (int i = 0; i < NumberofNumbers; i++)
+                {
+                    for (int ia = i + 1; ia < NumberofNumbers; ia++)
+                    {
+
+                        if (UnsortedNumbers[i] > UnsortedNumbers[ia  ])
+                        {
+                            int Temp = UnsortedNumbers[i];
+                            UnsortedNumbers[i] = UnsortedNumbers[ia];
+                            UnsortedNumbers[ia] = Temp;
+
+                        }
+                        Steps++;
+                    }
+
+                }
+
+            }
+
+
+
+            if (comboBox1.Text== "Insertion Sort")
+            {
+                theoSteps = NumberofNumbers * (NumberofNumbers - 1) / 2;
+
+                for (int i = 1; i < NumberofNumbers; i++)
+                {
+                    for (int ia = i; ia >=1; ia--)
+                    {
+
+                        if (UnsortedNumbers[ia] < UnsortedNumbers[ia-1])
+                        {
+                            int Temp = UnsortedNumbers[ia];
+                            UnsortedNumbers[ia] = UnsortedNumbers[ia - 1];
+                            UnsortedNumbers[ia - 1] = Temp;
+
+                        }
+                        Steps++;
+                    }
+
+                }
+
+            }
+
+
+
+            if (comboBox1.Text=="Bubble Sort")
+            {
+                theoSteps = NumberofNumbers * (NumberofNumbers - 1) / 2;
+
+                for (int i=1;i< NumberofNumbers;i++)
+                {
+                    for (int ia=0; ia<NumberofNumbers-i;ia++)
+                    {
+
+                        if (UnsortedNumbers[ia+1]< UnsortedNumbers[ia ])
+                        {
+                            int Temp = UnsortedNumbers[ia];
+                            UnsortedNumbers[ia] = UnsortedNumbers[ia + 1];
+                            UnsortedNumbers[ia + 1] = Temp;
+
+                        }
+                        Steps++;
+                    }
+
+
+                }
+
+
+
+
+            }
+            textBox2.Text = "";
+            for (int i = 0; i < NumberofNumbers; i++)
+            {
+
+               
+                textBox2.Text += UnsortedNumbers[i].ToString() + "\t";
+
+            }
+
+            textBox3.Text = Steps.ToString();
+            textBox4.Text = theoSteps.ToString();
+            
+        }
+    }
+}
